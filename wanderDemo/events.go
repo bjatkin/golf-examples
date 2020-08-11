569d34e14d14b564309a4f9a05861bfcfbbf4237
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-08-11 11:27:52 -0600

    make sure to initalize the storyEventHandler

--- wanderDemo/events.go
+++ wanderDemo/events.go
@@ -4,20 +4,28 @@ type event int
 
 const (
 	twoFiftySixSecondsPassed = iota
+	talkToCountKidAfterFinished
+	talkedToWellGuy
+	talkedToOldGuy
+	talkedToDog
 )
 
 var storyEventHandler = eventHandler{}
 
 type eventHandler struct {
-	handlers map[event][]*func()
+	handlers map[event][]func()
+}
+
+func initEventHandler() {
+	storyEventHandler.handlers = make(map[event][]func())
 }
 
 func (h *eventHandler) sendEvent(e event) {
 	for _, f := range h.handlers[e] {
-		(*f)()
+		f()
 	}
 }
 
-func (h *eventHandler) onEvent(e event, handler *func()) {
+func (h *eventHandler) onEvent(e event, handler func()) {
 	h.handlers[e] = append(h.handlers[e], handler)
 }
