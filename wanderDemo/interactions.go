bf036c5153309707ea5dd9131fc0652edd5da6a0
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-08-11 11:28:40 -0600

    create an interactions system

--- wanderDemo/interactions.go
+++ wanderDemo/interactions.go
@@ -1,6 +1,17 @@
 package main
 
-import "fantasyConsole/golf"
+import (
+	"fantasyConsole/golf"
+	"math"
+)
+
+type interactable interface {
+	drawInteractable(vec2)
+	resetInteraction()
+	interact(vec2) bool
+	next() bool
+	line() string
+}
 
 type interaction struct {
 	location    vec2
@@ -9,7 +20,41 @@ type interaction struct {
 	cache       string
 }
 
-func (i *interaction) reset() {
+var mainInteraction interactable
+
+func (i *interaction) drawInteractable(player vec2) {
+	dx := math.Abs(i.location.x - player.x)
+	dy := math.Abs(i.location.y - player.y)
+	if dx > 30 || dy > 30 {
+		return
+	}
+
+	o := golf.SOp{TCol: golf.Col6, W: 2, H: 2}
+	x := i.location.x
+	y := i.location.y
+	switch (g.Frames() / 20) % 4 {
+	case 0:
+		g.Spr(131, x, y-22, o)
+	case 1:
+		g.Spr(133, x, y-23, o)
+	case 2:
+		g.Spr(135, x, y-22, o)
+	case 3:
+		g.Spr(133, x, y-23, o)
+	}
+	return
+}
+
+func (i *interaction) interact(iPoint vec2) bool {
+	dx := math.Abs(i.location.x - iPoint.x)
+	dy := math.Abs(i.location.y - iPoint.y)
+	if dx < 16 && dy < 16 {
+		return true
+	}
+	return false
+}
+
+func (i *interaction) resetInteraction() {
 	i.cache = ""
 	i.currentLine = 0
 }
@@ -33,15 +78,15 @@ var guardInter = interaction{
 }
 
 var oldManInter = interaction{
-	lines: []int{6, 7, 8, 9},
+	lines: []int{6, 7, 33, 8, 34, 9},
 }
 
-var ladyInter = interaction{
+var bootLadyInter = interaction{
 	lines: []int{11},
 }
 
 var wellManInter = interaction{
-	lines: []int{12, 13},
+	lines: []int{12, 37, 13},
 }
 
 var talkingWellInter = interaction{
@@ -58,7 +103,7 @@ var rentGuyInter = interaction{
 }
 
 var talkingDogInter = interaction{
-	lines: []int{21, 22, 23},
+	lines: []int{21, 22, 23, 35},
 }
 
 var countingKidInter = interaction{
@@ -67,19 +112,36 @@ var countingKidInter = interaction{
 
 var townSignInter = interaction{
 	location: vec2{129, 190},
-	lines:    []int{31},
+	lines:    []int{31, 32},
 }
 
-func drawInteractionSign(spot vec2) {
-	o := golf.SOp{TCol: golf.Col6, W: 2, H: 2}
-	switch (g.Frames() / 20) % 4 {
-	case 0:
-		g.Spr(131, spot.x, spot.y-22, o)
-	case 1:
-		g.Spr(133, spot.x, spot.y-23, o)
-	case 2:
-		g.Spr(135, spot.x, spot.y-22, o)
-	case 3:
-		g.Spr(133, spot.x, spot.y-23, o)
+func updateInteraction() {
+	if g.Btnp(golf.ZKey) {
+		if mainInteraction.next() {
+			return
+		}
+
+		g.Update = update
+		g.Draw = draw
+
+		if mainInteraction == &oldMan {
+			storyEventHandler.sendEvent(talkedToOldGuy)
+		}
+		if mainInteraction == &talkingDog {
+			storyEventHandler.sendEvent(talkedToDog)
+		}
+		if mainInteraction == &wellMan {
+			storyEventHandler.sendEvent(talkedToWellGuy)
+		}
+		if mainInteraction == &countingKid && g.Frames()/30 >= 253 {
+			storyEventHandler.sendEvent(talkToCountKidAfterFinished)
+		}
 	}
+}
+
+func drawInteraction() {
+	g.RectFill(0, 150, 192, 42, golf.Col0, true)
+	g.RectFill(1, 151, 190, 40, golf.Col7, true)
+	g.Rect(2, 152, 188, 38, golf.Col0, true)
+	g.Text(4, 158, mainInteraction.line(), golf.TOp{Fixed: true})
 }
