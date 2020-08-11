4d8efded09b0d27b9a36ffc01ceb0f985eee2da1
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-08-11 11:29:45 -0600

    add interactables to the sceans

--- wanderDemo/scean.go
+++ wanderDemo/scean.go
@@ -7,6 +7,7 @@ type vec2 struct {
 }
 
 var mainScean *scean
+var prevScean *scean
 
 func exitScean(player vec2) (*scean, bool) {
 	for s, pt := range mainScean.entrances {
@@ -19,34 +20,89 @@ func exitScean(player vec2) (*scean, bool) {
 	return nil, false
 }
 
+func interactWithScean(player vec2) (interactable, bool) {
+	x, y := 0.0, 0.0
+	if playerFacing == facingUp {
+		y = -8
+	}
+	if playerFacing == facingDown {
+		y = 24
+	}
+	if playerFacing == facingLeft {
+		x = -8
+	}
+	if playerFacing == facingRight {
+		x = 24
+	}
+	iPoint := vec2{player.x + x, player.y + y}
+	for _, poi := range mainScean.poi {
+		if poi.interact(iPoint) {
+			return poi, true
+		}
+	}
+	return nil, false
+}
+
 type scean struct {
 	mapXY     vec2
 	mapWH     vec2
 	entrances map[*scean][2]vec2
-	poi       []*interaction
+	poi       []interactable
+	collide   []collidable
 }
 
 var rentHouse = scean{
 	mapXY: vec2{52, 0},
 	mapWH: vec2{5, 7},
+	poi: []interactable{
+		&rentGuy,
+	},
+	collide: []collidable{
+		&rentGuy,
+	},
 }
 
 var dogHouse = scean{
 	mapXY: vec2{57, 0},
 	mapWH: vec2{10, 13},
+	poi: []interactable{
+		&talkingDog,
+	},
+	collide: []collidable{
+		&talkingDog,
+	},
 }
 
 var bootHouse = scean{
 	mapXY: vec2{67, 0},
 	mapWH: vec2{11, 9},
+	poi: []interactable{
+		&bootLady,
+		&oldMan,
+	},
+	collide: []collidable{
+		&bootLady,
+		&oldMan,
+	},
 }
 
 var townScean = scean{
 	mapXY: vec2{0, 0},
 	mapWH: vec2{52, 37},
-	poi: []*interaction{
+	poi: []interactable{
 		&townSignInter,
-		&talkingWellInter,
+		&guard1,
+		&guard2,
+		&fishKid,
+		&wellMan,
+		&countingKid,
+	},
+	collide: []collidable{
+		&guard1,
+		&guard2,
+		&fishKid,
+		&wellMan,
+		&countingKid,
 	},
 }
 
@@ -58,9 +114,12 @@ func initSceans() {
 	bootHouse.entrances = make(map[*scean][2]vec2)
 	rentHouse.entrances = make(map[*scean][2]vec2)
 
+	// Update interactables on event
+	storyEventHandler.onEvent(talkedToWellGuy, func() { townScean.poi = append(townScean.poi, &talkingWellInter) })
+
 	mainScean = &townScean
 	linkSceans(&townScean, vec2{136, 96}, vec2{136, 102}, &dogHouse, vec2{16, 88}, vec2{16, 78})
-	linkSceans(&townScean, vec2{296, 126}, vec2{295, 132}, &bootHouse, vec2{0, 55}, vec2{0, 44})
+	linkSceans(&townScean, vec2{296, 126}, vec2{295, 136}, &bootHouse, vec2{0, 55}, vec2{0, 44})
 	linkSceans(&townScean, vec2{104, 94}, vec2{104, 102}, &rentHouse, vec2{0, 42}, vec2{12, 42})
 }
 
@@ -75,10 +134,6 @@ func (s *scean) draw() {
 
 func (s *scean) drawPOI(player vec2) {
 	for _, poi := range s.poi {
-		dx := math.Abs(poi.location.x - player.x)
-		dy := math.Abs(poi.location.y - player.y)
-		if dx < 30 && dy < 30 {
-			drawInteractionSign(poi.location)
-		}
+		poi.drawInteractable(playerXY)
 	}
 }
